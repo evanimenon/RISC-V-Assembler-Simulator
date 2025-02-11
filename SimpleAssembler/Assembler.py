a57720b80be2be93110c5b1b8e365e197cde9d4d
Chitraksh Bhatia <chitraksh24164@iiitd.ac.in>
2025-02-11 17:04:30 +0530

    Started the extraction of data from file

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -1,1 +1,10 @@
-chitraksh has a pepsi addiction
+
+def extract(file_name):
+    f = open(file_name,"r")
+    for line in f:
+        operation = line.split(" ")[0].strip()
+        right = line.split(" ")[1].strip()
+        print(operation," ",right)
+    f.close()
+
+extract("Ex_test_5")
