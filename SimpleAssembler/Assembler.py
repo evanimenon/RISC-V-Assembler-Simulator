4f681a6509f5902ca143c73f1fc04f9f3de7c00a
Chitraksh Bhatia <chitraksh24164@iiitd.ac.in>
2025-02-13 12:18:42 +0530

    Created function to write final output to file, added registers, found bugs

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -66,15 +66,13 @@ def int_to_binary(value, bit_width):
     
 
 def get_instruction_type(instr,data):
-    Binary_Instruction = []
     PC = ''
     #Checks which type the instruction belongs to
     #note to self: instr and data are both strings
     if instr in riscv_r_type:
         Operands = data.split(',')
-        PC += riscv_r_type[instr]['func7'] + ' ' + riscv_registers[Operands[2]] + ' ' + riscv_registers[Operands[1]] + ' ' + riscv_r_type[instr]['func3'] + ' ' + riscv_registers[Operands[0]] + ' ' + riscv_r_type[instr]['opcode']
+        PC += riscv_r_type[instr]['func7'] + riscv_registers[Operands[2]] + riscv_registers[Operands[1]] + riscv_r_type[instr]['func3'] + riscv_registers[Operands[0]] + riscv_r_type[instr]['opcode']
         #Above line is for conversion
-        Binary_Instruction.append(PC)
 
     elif instr in riscv_i_type:
         info = data.split(',')
@@ -82,12 +80,11 @@ def get_instruction_type(instr,data):
         if instr in "JALRLW":
             Operands[1] = info[1].split('(')[1].strip(')')
             Operands[2] = int_to_binary(int(info[1].split('(')[0]),12)
-            PC += Operands[2] + ' ' + riscv_registers[Operands[1]] + ' ' + riscv_i_type[instr]['func3'] + ' ' + riscv_registers[Operands[0]] + ' ' + riscv_i_type[instr]['opcode']
-            Binary_Instruction.append(PC)
+            PC += Operands[2] + riscv_registers[Operands[1]] + riscv_i_type[instr]['func3'] + riscv_registers[Operands[0]] + riscv_i_type[instr]['opcode']
         else:
             Operands[1] = info[1]
             Operands[2] = int_to_binary(int(info[2]),12)
-            PC += Operands[2] + ' ' + riscv_registers[Operands[1]] + ' ' + riscv_i_type[instr]['func3'] + ' ' + riscv_registers[Operands[0]] + ' ' + riscv_i_type[instr]['opcode']
+            PC += Operands[2] + riscv_registers[Operands[1]] + riscv_i_type[instr]['func3'] + riscv_registers[Operands[0]] + riscv_i_type[instr]['opcode']
 
     elif instr in riscv_s_type:
         rs2, offset_rs1 = data.split(',')
@@ -97,24 +94,21 @@ def get_instruction_type(instr,data):
         imm_bin = int_to_binary(int(offset), 12)
         imm_high, imm_low = imm_bin[:7], imm_bin[7:]
 
-        PC += (imm_high + ' ' + riscv_registers[rs2] + ' ' + riscv_registers[rs1] + ' ' +
-               riscv_s_type[instr]['func3'] + ' ' + imm_low + ' ' + riscv_s_type[instr]['opcode'])
-        Binary_Instruction.append(PC)
+        PC += (imm_high + riscv_registers[rs2] + riscv_registers[rs1] +
+               riscv_s_type[instr]['func3'] + imm_low + riscv_s_type[instr]['opcode'])
 
     elif instr in riscv_b_type:
         rs1, rs2, offset = data.split(',')
         imm_bin = int_to_binary(int(offset), 12)
         imm_high, imm_low = imm_bin[:7], imm_bin[7:]
 
-        PC += (imm_high + ' ' + riscv_registers[rs2] + ' ' + riscv_registers[rs1] + ' ' + 
-               riscv_b_type[instr]['func3'] + ' ' + imm_low + ' ' + riscv_b_type[instr]['opcode'])
-        Binary_Instruction.append(PC)
+        PC += (imm_high + riscv_registers[rs2] + riscv_registers[rs1] + 
+               riscv_b_type[instr]['func3'] + imm_low + riscv_b_type[instr]['opcode'])
 
     elif instr in riscv_u_type:
         rd, imm = data.split(',')
         imm_bin = int_to_binary(int(imm), 20)   	
-        PC += imm_bin + ' ' + riscv_registers[rd] + ' ' + riscv_u_type[instr]['opcode']
-        Binary_Instruction.append(PC)
+        PC += imm_bin + riscv_registers[rd] + riscv_u_type[instr]['opcode']
         
     elif instr in riscv_j_type:
         rd, imm = data.split(',')
@@ -123,9 +117,8 @@ def get_instruction_type(instr,data):
         imm_high = imm_bin[1:11]
         imm_sep  = imm_bin[11]
         imm_low  = imm_bin[12:20]
-        PC += imm_msb + ' ' + imm_high + ' ' + imm_sep + ' ' + imm_low + ' ' + riscv_registers[rd] + ' ' + riscv_j_type[instr]['opcode']
-        Binary_Instruction.append(PC)
-        
+        PC += imm_msb + imm_high + imm_sep + imm_low + riscv_registers[rd] + riscv_j_type[instr]['opcode']
+
     else:
         return "UNKNOWN"
     return PC
@@ -138,9 +131,12 @@ riscv_registers = {
     "x16": "10000", "x17": "10001", "x18": "10010", "x19": "10011",
     "x20": "10100", "x21": "10101", "x22": "10110", "x23": "10111",
     "x24": "11000", "x25": "11001", "x26": "11010", "x27": "11011",
-    "x28": "11100", "x29": "11101", "x30": "11110", "x31": "11111"
+    "x28": "11100", "x29": "11101", "x30": "11110", "x31": "11111",
+    "zero":"00000", "ra":"00001", "sp":"00010", "gp":"00011", "tp":"00100",
+    "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010",
+    "a1":"01011", "a2":"01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4":"10100",
+    "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10":"11010", "s11":"11011", "t3":"11100", "t4":"11101", "t5":"11110", "t6":"11111"
 }
-
 
 def extract(file_name):
     main={} # general structure to be {operation:data}
@@ -148,14 +144,25 @@ def extract(file_name):
     for line in f:
         operation = line.split(" ")[0].strip()
         data = line.split(" ")[1].strip()
-        main[operation] = data
-    for i,j in main.items():
-        print(i,j)
+        main[operation.strip().upper()] = data
     f.close()
     return main
 
-main = extract("instruction.txt")
+def write(main):
+    f = open("binary.txt",'w')
+    for i,j in main.items():
+        bin = get_instruction_type(i,j)
+        print(bin)
+        f.write(bin) 
+        f.write("\n")
+    f.close()
+    return main
+
+
+main = extract("Ex_test_1.txt")
+
 count = 1
-for key in main:
-    print(count,' ' + get_instruction_type(key,main[key]))
+for i,j in main.items():
+    print(i,"-",j)
     count+=1
+write(main)
